b5281d0c946658dd2c1dfae8e282d55c83573674
sitaram kawade <sitaram.kawade@gmail.com>
2020-12-06 22:07:16 +0530

    My First Commit

---
+++ app/Http/Controllers/test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class test extends Controller
+{
+    //
+    public function show(Request $request)
+    {
+       
+        return view(t);
+
+    }
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+  <form action="t" method="post">
+      @csrf
+   Enter Text <input type="text" name ="t1" >
+Enter Text <input type="text" name ="t2" >
+
+<input type="submit">
+  </form>
+ 
+    
+</body>
+</html>
